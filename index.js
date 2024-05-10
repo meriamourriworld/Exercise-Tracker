94a80583e11bb55f760cbf48fbb564ed29ff8a29
Meriam Ourri <meriamourri@gmail.com>
2024-05-10 12:16:08 +0000

    Added an Exercise

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 require('dotenv').config();
 const connect = require("./models/connection");
 const User = require("./models/user");
-
+const Exercise = require("./models/exercice");
 
 //Configuration
 app.use(cors())
@@ -25,13 +25,27 @@ app.post("/api/users", async (req, res)=>
   const user = await User.findOne({username: username});
   res.json({"username":user.username , "_id": user._id});
 });
-
 app.get("/api/users", async (req, res)=>
 {
   const usersList = await User.find({});
   res.json(usersList);
 });
 
+
+app.post("/api/users/:_id/exercises", async (req, res)=>
+{
+  const {_id} = req.params;
+  const {description, duration, date} = req.body;
+  const {username} = await User.findById(_id);
+  if(date)
+  {
+    await Exercise.insertMany({username:username, description: description, duration: duration, date:date});
+  }else
+  {
+    await Exercise.insertMany({username:username, description: description, duration: duration});
+  }
+
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
---
+++ models/exercice.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
+
+const exerciseSchema = new Schema({
+    username: 
+    {
+        type: String,
+        required : true
+    },
+    description :
+    {
+        type: String,
+        required : true
+    },
+    duration:
+    {
+        type: Number,
+        required : true
+    },
+    date:
+    {
+        type: Date,
+        default : new Date().toDateString()
+    }
+});
+
+module.exports = mongoose.model("Exercise", exerciseSchema);
